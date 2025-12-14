c4a17c63ced7ebcc88418d2c0fab04936d3edddd
Jovan Milosevic <jovanm.nis555@gmail.com>
2025-12-14 20:13:42 +0100

    drugi commit

--- PPVezba1/src/KWTable.java
+++ PPVezba1/src/KWTable.java
@@ -2,13 +2,12 @@ import java.util.Hashtable;
 
 public class KWTable {
 
-    private Hashtable<String, Integer> mTable; // Dobra praksa je koristiti generike
+    private Hashtable<String, Integer> mTable; 
 
     public KWTable() {
-        // Inicijalizacija hash tabele koja pamti kljucne reci
+        
         mTable = new Hashtable<>();
         
-        // Kljucne reci iz VASEG zadatka
         mTable.put("program", sym.PROGRAM);
         mTable.put("return", sym.RETURN);
         mTable.put("begin", sym.BEGIN);
@@ -22,16 +21,13 @@ public class KWTable {
         mTable.put("in", sym.IN);
     }
 
-    /**
-     * Vraca ID kljucne reci 
-     */
+    
     public int find(String keyword) {
         Object symbol = mTable.get(keyword);
         if (symbol != null) {
-            return (Integer) symbol; // Moderniji Java kod automatski radi unboxing
+            return (Integer) symbol; 
         }
         
-        // Ako rec nije pronadjena u tabeli kljucnih reci radi se o identifikatoru
         return sym.ID;
     }
 }
